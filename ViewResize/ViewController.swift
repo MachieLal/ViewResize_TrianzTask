26952f1555ab4e57fc701c491394039344448da7
SwarupPattnaik <swarup.pattnaik@evry.com>
2017-05-03 17:00:40 +0530

    Initial Commit

---
+++ ViewResize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewResize
+//
+//  Created by Swarup-Pattnaik on 5/3/17.
+//  Copyright © 2017 Swarup-Pattnaik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
